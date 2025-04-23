e7ee5b1d5bf8230ab63d5366552748588bcdf4d9
Kerwin Hy <kerwin.hy@gmail.com>
2025-04-22 21:45:28 -0700

    update: adjust views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.derpy.derpynotes"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.00"
+        versionCode = 3
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
